a9dfe6125a0f125f51b3d7202034b04ac63f0979
Ryoung27 <RRYoung89@gmail.com>
2019-01-02 14:09:58 -0600

    updated title

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Title
+let title = document.createElement("title");
+title.innerHTML= "R | YOUNG"
+document.head.appendChild(title);
+
+// let navigation = document.createElement("div");
+// navigation.innerHTML="hello there"
+// document.body.appendChild(navigation);
+
+
+
