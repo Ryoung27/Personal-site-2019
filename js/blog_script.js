3d23f26bec8bf7c246d05aeeebc5ac849ffaf541
Ryoung27 <RRYoung89@gmail.com>
2019-09-21 16:30:36 -0500

    blog section added.

---
+++ js/blog_script.js
@@ -0,0 +1,4 @@
+//Title
+let title = document.createElement("title");
+title.innerHTML= "R | BLOG"
+document.head.appendChild(title);
--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ let title = document.createElement("title");
 title.innerHTML= "R | YOUNG"
 document.head.appendChild(title);
 
+
+
 // let navigation = document.createElement("div");
 // navigation.innerHTML="hello there"
 // document.body.appendChild(navigation);
